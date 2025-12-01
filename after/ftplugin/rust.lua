49a5b1cd479730528ab8fac3587b23ab1ffcdb0e
cogs <cogs@springless.com>
2025-12-01 12:55:18 -0600

    Add bindings to Rust `after` file directly
    Since Rust is now being instantiated somewhat differently the key
    bindings that are usually added as part of Mason's initialization
    procedures are no longer being added. This adds those keybinds directly
    to the rust after attach file.

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -16,3 +16,26 @@ vim.keymap.set(
   end,
   { silent = true, buffer = bufnr }
 )
+
+
+-- mappings
+-- see `:help vim.lsp.*` for documentation
+-- settings from here: https://github.com/neovim/nvim-lspconfig#keybindings-and-completion
+local bufopts = { noremap=true, silent=true, buffer=bufnr }
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+--vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, bufopts)
+--vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+--vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+--vim.keymap.set('n', '<space>wl', function()
+--  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+--end, bufopts)
+--vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+--vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+--vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+--vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+-- Pop open diagnostics window for LSP errors
+vim.keymap.set('n', '<C-k>', vim.diagnostic.open_float, bufopts)
