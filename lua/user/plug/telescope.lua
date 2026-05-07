b25170dd631f1611be7e287a7d7273ef10e7cde8
cogs <cogs@springless.com>
2026-05-07 17:44:51 -0500

    Scope ff and fg to the cwd of NvimTree

--- lua/user/plug/telescope.lua
+++ lua/user/plug/telescope.lua
@@ -1,11 +1,18 @@
+local function nvimtree_root()
+  local ok, core = pcall(require, "nvim-tree.core")
+  if ok and core.get_cwd then
+    return core.get_cwd()
+  end
+end
+
 local function setup()
   require("telescope").setup()
 
   local keymap = vim.keymap.set
   local opts = { noremap = true, silent = true }
   local builtin = require("telescope.builtin")
-  keymap("n", "<leader>ff", builtin.find_files, opts)
-  keymap("n", "<leader>fg", builtin.live_grep, opts)
+  keymap("n", "<leader>ff", function() builtin.find_files({ cwd = nvimtree_root() }) end, opts)
+  keymap("n", "<leader>fg", function() builtin.live_grep({ cwd = nvimtree_root() }) end, opts)
   keymap("n", "<leader>fb", builtin.buffers, opts)
   keymap("n", "<leader>fh", builtin.help_tags, opts)
 end
